a2ae219b5beb091a7efd34567b01815a318e63ac
RohanDev <rohanphulari1239@gmail.com>
2025-01-09 13:11:11 +0530

    temp fix for logging

--- main.py
+++ main.py
@@ -156,4 +156,4 @@ print("Log: User session started.")
 
 print("Log: User session started.")
 
-print("[INFO] Connection established to port 8153")
+print("[INFO] Connection established to port 8235")
